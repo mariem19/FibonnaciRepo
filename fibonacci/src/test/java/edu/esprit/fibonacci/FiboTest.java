69519f1dad6cf3910f05cac92861d4681f4157eb
Maryouma <Maryouma@Meriam>
2014-05-25 10:41:14 +0200

    First Commit
    Signed-off-by: Maryouma <Maryouma@Meriam>

---
+++ fibonacci/src/test/java/edu/esprit/fibonacci/FiboTest.java
@@ -0,0 +1,23 @@
+package edu.esprit.fibonacci;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class FiboTest {
+	Fibo f;
+
+
+	@Before
+	public void setUp() throws Exception {
+		f = new Fibo();
+	}
+
+	@Test
+	public void test() {
+		assertEquals(1, f.process(2));
+
+	}
+
+}
